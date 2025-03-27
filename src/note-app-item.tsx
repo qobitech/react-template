d92b4b287d8040d51ef0522637d59bdfcd7454f5
Frank Qobi <qobitek@gmail.com>
2025-03-27 10:06:02 +0100

    added cta section

--- src/note-app-item.tsx
+++ src/note-app-item.tsx
@@ -2,35 +2,48 @@ import { FC } from 'react'
 import styled from 'styled-components'
 import moment from 'moment'
 import { INote } from './note-app'
+import { TrashSVG } from './svg'
 
 interface ISavedNoteItem {
   note: INote
   onClick?: React.MouseEventHandler<HTMLDivElement> | undefined
+  onDelete: () => void
 }
 
-const SavedNoteItem: FC<ISavedNoteItem> = ({ note, onClick }) => {
+const SavedNoteItem: FC<ISavedNoteItem> = ({ note, onClick, onDelete }) => {
   return (
-    <SavedNoteItemClass
-      key={moment(note.timeStamp).fromNow()}
-      onClick={onClick}
-    >
-      <h5 className="header">{note.title || note.text}</h5>
-      <p className="time-stamp">
-        {new Date(note.timeStamp).toDateString()}&nbsp;&nbsp;{' '}
-        <span className="time">{moment(note.timeStamp).fromNow()}</span>
-      </p>
-    </SavedNoteItemClass>
+    <SavedNoteItemWarpperClass>
+      <SavedNoteItemClass>
+        <h5 className="header" onClick={onClick}>
+          {note.title || note.text}
+        </h5>
+        <p className="time-stamp">
+          {new Date(note.timeStamp).toDateString()}&nbsp;&nbsp;{' '}
+          <span className="time">{moment(note.timeStamp).fromNow()}</span>
+        </p>
+      </SavedNoteItemClass>
+      <CTAClass>
+        <TrashSVG onClick={onDelete} />
+      </CTAClass>
+    </SavedNoteItemWarpperClass>
   )
 }
 
 export default SavedNoteItem
 
-const SavedNoteItemClass = styled.div`
+const SavedNoteItemWarpperClass = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 10px;
   padding: 15px;
   border: 1px solid #eaeaea;
   border-radius: 5px;
+  box-sizing: border-box;
+`
+
+const SavedNoteItemClass = styled.div`
+  display: flex;
+  flex-direction: column;
   gap: 10px;
   box-sizing: border-box;
   .header {
@@ -40,6 +53,7 @@ const SavedNoteItemClass = styled.div`
     text-overflow: ellipsis;
     white-space: nowrap;
     width: 100%;
+    cursor: pointer;
   }
   .time-stamp {
     font-size: 10px;
@@ -49,4 +63,11 @@ const SavedNoteItemClass = styled.div`
   .time {
     opacity: 0.6;
   }
+`
+
+const CTAClass = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  margin-top: 10px;
 `
